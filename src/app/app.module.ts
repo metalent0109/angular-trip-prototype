ad3009327cffcbdfafc008353d95bf29430359ba
Aleksandar Rakić <aleksandar.rakic@yahoo.com>
2022-09-01 18:21:37 +0200

    work in progress [2022-09-01]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
+import {FormsModule} from "@angular/forms";
+import {SignUpComponent} from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
